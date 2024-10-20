f261a925e8ec0b710141868edd2b40bce18f1e09
AmineAbdessayed <mohamedamine.abdessayed@esprit.tn>
2024-10-20 06:47:19 +0100

    3

--- Event/admin.py
+++ Event/admin.py
@@ -5,7 +5,9 @@ from .models import Event
 
 # Register your models here.
 class EventAdmin(admin.ModelAdmin):
-    list_display=['title','evt_date','state','nbr_participant']
+    list_display = ['title', 'evt_date', 'state', 'nbr_participant', 'creation_date', 'update_date']
+    readonly_fields=['creation_date','update_date']
+    list_per_page=2
     
     
     
